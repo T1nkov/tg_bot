8e6c233ae14e334306eb562170a2ead458eb7b91
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-12 14:15:32 +0300

    copy paste from source to dist

--- Tg_Bot_from_t1nkov.php
+++ Tg_Bot_from_t1nkov.php
@@ -183,7 +183,7 @@ switch ($text) {
 		$db->setInputMode($chat_id, 'def');
 		$db->handleMainMenu($telegram, $chat_id);
 		break;
-	case ($text != null):
+	case ($text != null): // Проверяем, что текст не null
 		if ($db->isInputMode($chat_id) == 'input_mode') {
 			$params = [
 				'chat_id' => $chat_id,
