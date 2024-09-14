9dbbd39cb9848dffe69803d0915edf95a2372c56
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-14 13:06:22 +0300

    fix syntax

--- class/SubscribeLogic.php
+++ class/SubscribeLogic.php
@@ -31,7 +31,7 @@ trait SubscribeLogic {
         $response = $response = $telegram->getChatMember(['chat_id' => $tg_key, 'user_id' => $chat_id]);
         $subscriptionStatus = $response['result']['status'];
         if ($subscriptionStatus === 'member' || $subscriptionStatus === 'administrator' || $subscriptionStatus === 'creator') {
-            this->incrementBalance($chat_id, $GLOBALS['subscribeSumValue'])
+            this->incrementBalance($chat_id, $GLOBALS['subscribeSumValue']);
             $message = "✅ Проверка прошла! {$GLOBALS['subscribeSumValue']}\nОставайтесь активными и не отписывайтесь от канала в течение 5 дней. Если вы отпишетесь, деньги вернутся.";
             $keyboard = json_encode(['inline_keyboard' => [[['text' => 'Next', 'callback_data' => 'next']]]]);
         } else {
