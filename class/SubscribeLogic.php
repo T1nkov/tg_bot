03c148259a5879a38eddce6589a1052a2de01ce9
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-14 11:18:43 +0300

    try fetch

--- class/SubscribeLogic.php
+++ class/SubscribeLogic.php
@@ -2,32 +2,60 @@
 trait SubscribeLogic {
 
     public function handleJoinChannelCommand($telegram, $chat_id, $message_id) {
-		$tg_key = $this->getKey();
-		$channelURL = $this->getURL($tg_key);
-		$handleMessage = $this->getPhraseText("join_text", $chat_id);
-		$message = str_replace(
-			['{$sum}', '{$chanURL}'],
-			[$GLOBALS['joinChannelPay'], $channelURL],
-			$handleMessage
-		);
-		$keyboard = json_encode([
-			'inline_keyboard' => [
-				[['text' => $this->getPhraseText("checkChannel_button", $chat_id), 'callback_data' => 'check']],
-				[['text' => $this->getPhraseText("skipChannel_button", $chat_id), 'callback_data' => 'skip']]
-			]
-		]);		
-		$content = [
-			'chat_id' => $chat_id,
-			'message_id' => $message_id,
-			'text' => $message,
-			'reply_markup' => $keyboard
-		];
-		try {
-			$telegram->editMessageText($content);
-		} catch (Exception $e) {
-			error_log('Ошибка при редактировании сообщения: ' . $e->getMessage());
-		}
-	}
+        $tg_key = $this->getKey();
+        $channelURL = $this->getURL($tg_key);
+        $handleMessage = $this->getPhraseText("join_text", $chat_id);
+        $message = str_replace(
+            ['{$sum}', '{$chanURL}'],
+            [$GLOBALS['joinChannelPay'], $channelURL],
+            $handleMessage
+        );
+        $keyboard = json_encode([
+            'inline_keyboard' => [
+                [['text' => $this->getPhraseText("checkChannel_button", $chat_id), 'callback_data' => 'check']],
+                [['text' => $this->getPhraseText("skipChannel_button", $chat_id), 'callback_data' => 'skip']]
+            ]
+        ]);
+        $content = [
+            'chat_id' => $chat_id,
+            'message_id' => $message_id,
+            'text' => $message,
+            'reply_markup' => $keyboard
+        ];
+        try {
+            $telegram->editMessageText($content);
+        } catch (Exception $e) {
+            error_log('Ошибка при редактировании сообщения: ' . $e->getMessage());
+        }
+    }
+
+    public function checkUserSubscription($telegram, $chat_id, $user_id) {
+        $tg_key = $this->getKey();
+        try {
+            $response = $telegram->getChatMember($tg_key, $user_id);
+            $subscriptionStatus = $response->status;
+            if ($subscriptionStatus == 'member' || $subscriptionStatus == 'administrator' || $subscriptionStatus == 'creator') {
+                $message = "✅ Проверка прошла! {$GLOBALS['subscribeSumValue']}\nОставайтесь активными и не отписывайтесь от канала в течение 5 дней. Если вы отпишетесь, деньги вернутся.";
+            } else {
+                $channelURL = $this->getURL($tg_key);
+                $message = "❌ Проверить не удалось! Подпишитесь на канал: {$channelURL}";
+            }
+            $keyboard = json_encode([
+                'inline_keyboard' => [
+                    [['text' => $this->getPhraseText("checkChannel_button", $chat_id), 'callback_data' => 'check']],
+                    [['text' => $this->getPhraseText("skipChannel_button", $chat_id), 'callback_data' => 'skip']]
+                ]
+            ]);
+            $content = [
+                'chat_id' => $chat_id,
+                'text' => $message,
+                'reply_markup' => $keyboard
+            ];
+            $telegram->sendMessage($content);
+        } catch (Exception $e) {
+            error_log('Ошибка при проверке подписки: ' . $e->getMessage());
+        }
+    }
 
     private function getKey() {
         $sql = "SELECT tg_key FROM channel_tg LIMIT 1";
@@ -43,5 +71,4 @@ trait SubscribeLogic {
         $stmt->close();
         return null;
     }
-    
 }
