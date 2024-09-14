fdcd9180eb5801583959daadacfdd32ef2eb25b7
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-14 23:57:24 +0300

    handle

--- class/SubscribeLogic.php
+++ class/SubscribeLogic.php
@@ -13,28 +13,27 @@ trait SubscribeLogic {
             //     'text' => $message,
             //     'reply_markup' => $keyboard
             // ]); 
-        } else {
-            $channelURL = $this->getURL($tg_key);
-            $handleMessage = $this->getPhraseText("join_text", $chat_id);
-            $message = str_replace(
-                ['{$sum}', '{$chanURL}'],
-                [$GLOBALS['joinChannelPay'], $channelURL],
-                $handleMessage
-            );
-            $keyboard = json_encode([
-                'inline_keyboard' => [
-                    [['text' => $this->getPhraseText("checkChannel_button", $chat_id), 'callback_data' => 'check']],
-                    [['text' => $this->getPhraseText("skipChannel_button", $chat_id), 'callback_data' => 'skip']]
-                ]
-            ]);
-            $content = [
-                'chat_id' => $chat_id,
-                'message_id' => $message_id,
-                'text' => $message,
-                'reply_markup' => $keyboard
-            ];
-            $telegram->editMessageText($content);
         }
+        $channelURL = $this->getURL($tg_key);
+        $handleMessage = $this->getPhraseText("join_text", $chat_id);
+        $message = str_replace(
+            ['{$sum}', '{$chanURL}'],
+            [$GLOBALS['joinChannelPay'], $channelURL],
+            $handleMessage
+        );
+        $keyboard = json_encode([
+            'inline_keyboard' => [
+                [['text' => $this->getPhraseText("checkChannel_button", $chat_id), 'callback_data' => 'check']],
+                [['text' => $this->getPhraseText("skipChannel_button", $chat_id), 'callback_data' => 'skip']]
+            ]
+        ]);
+        $content = [
+            'chat_id' => $chat_id,
+            'message_id' => $message_id,
+            'text' => $message,
+            'reply_markup' => $keyboard
+        ];
+        $telegram->editMessageText($content);
     }
 
     public function handleSubscribeCommand($telegram, $chat_id, $message_id) {
