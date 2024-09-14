688dc758c17683f6e6198d73e9cda7ca1a1c73f2
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-14 08:07:04 +0300

    fix a bit

--- class/AdminPanel.php
+++ class/AdminPanel.php
@@ -77,7 +77,7 @@ trait AdminPanel {
         $maxId = $result->fetch_assoc()['max_id'];
         $newId = $maxId ? $maxId + 1 : 1;
         $stmt = $this->conn->prepare("INSERT INTO channel_tg (id, tg_key, tg_url) VALUES (?, ?, ?)");
-        $tgKey = $this->getChatIdByLink($telegram, $chat_id, $url)
+        $tgKey = $this->getChatIdByLink($telegram, $chat_id, $url);
         $stmt->bind_param("iss", $newId, $tgKey, $url);
         if ($stmt->execute()) {
             $telegram->sendMessage([
