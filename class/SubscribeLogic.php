6d3c3f98a3e753023897a6356ce63df4835bce7c
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-14 10:31:21 +0300

    return with few things

--- class/SubscribeLogic.php
+++ class/SubscribeLogic.php
@@ -22,28 +22,33 @@ trait SubscribeLogic {
         }
     }
 
-    public function handleJoinChannelCommand($telegram, $chat_id, $user_id) {
-        $nextChannel = $this->getNextChannel($chat_id);
-        if (!$nextChannel) {
-            $telegram->sendMessage([
-                'chat_id' => $chat_id,
-                'text' => "🥳 Вы подписались на все каналы!",
-            ]);
-            return;
-        }
-        $message = "Подпишись на канал и получи " . $GLOBALS['subscribeSumValue'];
-        $keyboard = [
-            'inline_keyboard' => [
-                [['text' => 'Проверить', 'callback_data' => 'check_subscription']],
-                [['text' => 'Пропустить', 'callback_data' => 'skip']]
-            ]
-        ];
-        $telegram->sendMessage([
-            'chat_id' => $chat_id,
-            'text' => $message,
-            'reply_markup' => json_encode($keyboard)
-        ]);
-    }
+    public function handleJoinChannelCommand($telegram, $chat_id, $message_id) {
+		$tg_key = this->getNextChannel($chat_id);
+		$channelURL = $this->getURL($tg_key);
+		$handleMessage = $this->getPhraseText("join_text", $chat_id);
+		$message = str_replace(
+			['{$sum}', '{$chanURL}'],
+			[$GLOBALS['joinChannelPay'], $channelURL],
+			$handleMessage
+		);
+		$keyboard = json_encode([
+			'inline_keyboard' => [
+				[['text' => $this->getPhraseText("checkChannel_button", $chat_id), 'callback_data' => 'check']],
+				[['text' => $this->getPhraseText("skipChannel_button", $chat_id), 'callback_data' => 'skip']]
+			]
+		]);		
+		$content = [
+			'chat_id' => $chat_id,
+			'message_id' => $message_id,
+			'text' => $message,
+			'reply_markup' => $keyboard
+		];
+		try {
+			$telegram->editMessageText($content);
+		} catch (Exception $e) {
+			error_log('Ошибка при редактировании сообщения: ' . $e->getMessage());
+		}
+	}
 
     public function handleCheckSubscription($telegram, $chat_id, $user_id) {
         $nextChannel = $this->getNextChannel($chat_id);
