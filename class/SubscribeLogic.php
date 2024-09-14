b2dbff7342da39ac6d1c3c00781b58b64e12bc3e
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-14 13:12:41 +0300

    check without increase balance

--- class/SubscribeLogic.php
+++ class/SubscribeLogic.php
@@ -33,7 +33,7 @@ trait SubscribeLogic {
         if ($subscriptionStatus === 'member' || $subscriptionStatus === 'administrator' || $subscriptionStatus === 'creator') {
             $message = "✅ Проверка прошла! {$GLOBALS['subscribeSumValue']}\nОставайтесь активными и не отписывайтесь от канала в течение 5 дней. Если вы отпишетесь, деньги вернутся.";
             $keyboard = json_encode(['inline_keyboard' => [[['text' => 'Next', 'callback_data' => 'next']]]]);
-            this->incrementBalance($chat_id, $GLOBALS['subscribeSumValue']);
+            // this->incrementBalance($chat_id, $GLOBALS['subscribeSumValue']);
         } else {
             $channelURL = $this->getURL($tg_key);
             $message = "❌ Проверить не удалось! Подпишитесь на канал: {$channelURL}";
