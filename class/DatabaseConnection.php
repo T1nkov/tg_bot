d362c47bba950c4e2d663378349b5cb077df0520
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-15 01:51:17 +0300

    handle String for DB referal_id

--- class/DatabaseConnection.php
+++ class/DatabaseConnection.php
@@ -143,7 +143,7 @@ class DatabaseConnection {
 		]);
 		try {
 			$status = 'def';
-			$stmt->bind_param("ssiids", $username, $role, $chat_id, $id_referal, $balance, $status);
+			$stmt->bind_param("sssdss", $username, $role, $chat_id, $id_referal, $balance, $status);
 		} catch (\Exception $e) {
 			$telegram->sendMessage([
 				'chat_id' => $chat_id,
