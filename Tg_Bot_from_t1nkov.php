56f6ac97b01a2dbe7f775845671444c617335273
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-14 04:57:34 +0300

    catch

--- Tg_Bot_from_t1nkov.php
+++ Tg_Bot_from_t1nkov.php
@@ -79,6 +79,10 @@ if (isset($commands[$callback_data])) {
     if ($callback_data === 'add_channel') {
         $db->handleUserInput($chat_id, $telegram);
         if($command != null) {
+            $telegram->sendMessage([
+                'chat_id' => $chat_id,
+                'text'    => 'Text from catch: ' . $command,
+            ]);
             $db->addUrlInDB($telegram, $chat_id, $command);
             $db->setInputMode($chat_id, 'def');
         }
