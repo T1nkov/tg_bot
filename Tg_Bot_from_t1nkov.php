ec432b91034b5ffbb281a0858388400bae8e6095
Dmitry Tinkov <mrfredyon@gmail.com>
2024-09-14 03:24:17 +0300

    add_url fix

--- Tg_Bot_from_t1nkov.php
+++ Tg_Bot_from_t1nkov.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 1);
 
 $config_file = require __DIR__ . '/config.php';
 
-if (!isset($config_file['db'])) { die("# db key error - Database configuration not found."); }
+if (!isset($config_file['db'])) {die("# db key error - Database configuration not found.");}
 
 $bot_token = $config_file['bot_token'];
 $telegram = new Telegram($bot_token);
@@ -17,42 +17,46 @@ $command = $telegram->Text();
 $chat_id = $telegram->ChatID();
 $data = $telegram->getData();
 
-$GLOBALS['adminHREF']         = 'https://t.me/t1nkov';
-$GLOBALS['currency']          = 'INR';
-$GLOBALS['amount']            = 500;
-$GLOBALS['inviteSumValue']    = 200 . $GLOBALS['currency'];
-$GLOBALS['cards']             = 10;
-$GLOBALS['ChannelID']         = 2248476665;
+$GLOBALS['adminHREF'] = 'https://t.me/t1nkov';
+$GLOBALS['currency'] = 'INR';
+$GLOBALS['amount'] = 500;
+$GLOBALS['inviteSumValue'] = 200 . $GLOBALS['currency'];
+$GLOBALS['cards'] = 10;
+$GLOBALS['ChannelID'] = 2248476665;
 $GLOBALS['subscribeSumValue'] = 1000 . $GLOBALS['currency'];
-$GLOBALS['watchSumValue']     = 8 . $GLOBALS['currency'];
-$GLOBALS['valueTg']           = 1;
-$GLOBALS['joinChannelPay']    = 1000 . $GLOBALS['currency'];
-$GLOBALS['adminID']           = 403480319;
-$GLOBALS['minWithdraw']       = number_format(10, 2, '.', '') . $GLOBALS['currency'];
-$GLOBALS['bonus']             = 10 . $GLOBALS['currency'];
+$GLOBALS['watchSumValue'] = 8 . $GLOBALS['currency'];
+$GLOBALS['valueTg'] = 1;
+$GLOBALS['joinChannelPay'] = 1000 . $GLOBALS['currency'];
+$GLOBALS['adminID'] = 403480319;
+$GLOBALS['minWithdraw'] = number_format(10, 2, '.', '') . $GLOBALS['currency'];
+$GLOBALS['bonus'] = 10 . $GLOBALS['currency'];
 $GLOBALS['buttons'] = [
     "ru" => ["🇷🇺 Русский"],
     "en" => ["🇺🇸 English"],
-    "kz" => ["🇰🇿 Қазақша"]
+    "kz" => ["🇰🇿 Қазақша"],
 ];
-  
+
 $update = json_decode(file_get_contents('php://input'), true);
 $callback_data = $update['callback_query']['data'] ?? null;
 $message_id = $update['callback_query']['message']['message_id'] ?? null;
 $GLOBALS['username1'] = $data['message']['from']['username'] ?? null;
 
-function isTextMatchingButtons($command) {
-	foreach ($GLOBALS['buttons'] as $buttonValues) {
-		if (in_array($command, $buttonValues)) return true;
-	}
-	return false;
+function isTextMatchingButtons($command)
+{
+    foreach ($GLOBALS['buttons'] as $buttonValues) {
+        if (in_array($command, $buttonValues)) {
+            return true;
+        }
+
+    }
+    return false;
 }
 
 $db = new DatabaseConnection($config_file);
 
 $telegram->sendMessage([
     'chat_id' => $chat_id,
-    'text'    => 'Callback data: ' . $callback_data,
+    'text' => 'Callback data: ' . $callback_data,
 ]);
 
 $commands = [
@@ -70,21 +74,19 @@ $commands = [
     'check' => 'handleSubscribeCheckCommand',
     'checkSub' => 'handleBalanceCommand',
     'no' => 'handleCanceledCommand',
-	'add_channel' => 'handleUserInput',
+    'add_channel' => 'handleUserInput',
     'remove_channel' => 'promptRemoveChannel',
-    'cancel_remove' => 'displayChannels'
+    'cancel_remove' => 'displayChannels',
 ];
 
 if (isset($commands[$callback_data])) {
     if ($commands[$callback_data] === 'add_channel') {
-		
-		$telegram->sendMessage([
-			'chat_id' => $chat_id,
-			'text'    => $this->isInputMode($chat_id)
-		]);
-        if($command != null && $this->isInputMode($chat_id)  == "input_mode"  ) {
-         $this-> addChannelURL($telegram, $chat_id, $command);
-		 $this ->setInputMode($chat_id, 'def');
+
+        if ($command != null) {
+            if ($this->isInputMode($chat_id) == "input_mode") {
+                $this->addChannelURL($telegram, $chat_id, $command);
+                $this->setInputMode($chat_id, 'def');
+            }
         }
     }
     $db->{$commands[$callback_data]}($telegram, $chat_id, $message_id, $bot_token ?? null);
@@ -95,8 +97,8 @@ if (isset($commands[$callback_data])) {
 }
 
 $telegram->sendMessage([
-	'chat_id' => $chat_id,
-	'text'    => 'Text: ' . $command,
+    'chat_id' => $chat_id,
+    'text' => 'Text: ' . $command,
 ]);
 
 switch ($command) {
@@ -111,46 +113,44 @@ switch ($command) {
             $db->handleMainMenu($telegram, $chat_id, $message);
         }
         break;
-	case $command !== null && (strpos($command, '/exit') === 0 || stripos($command, 'Выход') === 0):
-		$db->handleMainMenu($telegram, $chat_id);
-		break;
-	case $command !== null && strpos($command, 'Каналы') === 0:
-		if ($db->isAdmin($telegram, $chat_id)) { $db->displayChannels($telegram, $chat_id); }
-		break;
-	case isTextMatchingButtons($command):
-		$hhh = null;
-		foreach ($GLOBALS['buttons'] as $key => $values) {
-			if (in_array($command, $values)) {
-				$hhh = $key;
-				break;
-			}
-		}
-		if ($hhh !== null) { $db->updateUserLanguage($chat_id, $hhh); }
-		$db->handleLanguage($telegram, $chat_id);
-		break;
-	case $db->getPhraseText("button_earn", $chat_id):
-		$db->handleEarnCommand($telegram, $chat_id);
-		break;
-	case $db->getPhraseText("welcome_button", $chat_id):
-		$db->handleMainMenu($telegram, $chat_id);
-		break;
-	case $db->getPhraseText("button_balance", $chat_id):
-		$db->handleBalanceCommand($telegram, $chat_id, $bot_token);
-		break;
-	case $db->getPhraseText("button_partners", $chat_id):
-		$db->handlePartnerCommand($telegram, $chat_id);
-		break;
-	case $db->getPhraseText("button_changeLang", $chat_id):
-		$db->handleStartCommand($telegram, $chat_id, $update);
-		break;
-	case $db->getPhraseText("button_Help", $chat_id):
-		$db->handleHelpCommand($telegram, $chat_id);
-		break;
-	case $db->getPhraseText('download_button', $chat_id):
-		$db->handleDownloadCommand($telegram, $chat_id);
-		break;
-	default:
-		break;
+    case $command !== null && (strpos($command, '/exit') === 0 || stripos($command, 'Выход') === 0):
+        $db->handleMainMenu($telegram, $chat_id);
+        break;
+    case $command !== null && strpos($command, 'Каналы') === 0:
+        if ($db->isAdmin($telegram, $chat_id)) {$db->displayChannels($telegram, $chat_id);}
+        break;
+    case isTextMatchingButtons($command):
+        $hhh = null;
+        foreach ($GLOBALS['buttons'] as $key => $values) {
+            if (in_array($command, $values)) {
+                $hhh = $key;
+                break;
+            }
+        }
+        if ($hhh !== null) {$db->updateUserLanguage($chat_id, $hhh);}
+        $db->handleLanguage($telegram, $chat_id);
+        break;
+    case $db->getPhraseText("button_earn", $chat_id):
+        $db->handleEarnCommand($telegram, $chat_id);
+        break;
+    case $db->getPhraseText("welcome_button", $chat_id):
+        $db->handleMainMenu($telegram, $chat_id);
+        break;
+    case $db->getPhraseText("button_balance", $chat_id):
+        $db->handleBalanceCommand($telegram, $chat_id, $bot_token);
+        break;
+    case $db->getPhraseText("button_partners", $chat_id):
+        $db->handlePartnerCommand($telegram, $chat_id);
+        break;
+    case $db->getPhraseText("button_changeLang", $chat_id):
+        $db->handleStartCommand($telegram, $chat_id, $update);
+        break;
+    case $db->getPhraseText("button_Help", $chat_id):
+        $db->handleHelpCommand($telegram, $chat_id);
+        break;
+    case $db->getPhraseText('download_button', $chat_id):
+        $db->handleDownloadCommand($telegram, $chat_id);
+        break;
+    default:
+        break;
 }
-
-?>
