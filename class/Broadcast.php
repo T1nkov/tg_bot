41edf615bbf433e63dcfd5223c6a0ab33b9a85b1
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2024-09-15 08:34:40 +0300

    first view

--- class/Broadcast.php
+++ class/Broadcast.php
@@ -3,7 +3,19 @@
 trait Broadcast {
     
     public function displayPosts($telegram, $chat_id) {
-        
+        $message = "Ваши посты:\n" . $channelList;
+        $keyboard = [
+            'inline_keyboard' => [
+                [['text' => 'Начать Рассылку', 'callback_data' => 'init_cast']],
+                [['text' => 'Создать пост', 'callback_data' => 'create_post']]
+            ]
+        ];
+        $content = [
+            'chat_id' => $chat_id,
+            'text' => $message,
+            'reply_markup' => json_encode($keyboard)
+        ];
+        $telegram->sendMessage($content);
     }
 
     public function handleNewPost() {
