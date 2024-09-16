40563246a6434f3175b6c2b77a037157e9f14dd2
Wl0cKk <kandraciuk404@gmail.com>
2024-09-16 22:31:14 +0300

    first approach, miss file

--- src/broadcast_switch.sh
+++ src/broadcast_switch.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-CRON_COMMAND="/usr/bin/php /var/www/t.mercadalibre.live/broadcast_driver.php"
-CRON_COMMENT="# $CRON_COMMAND"
+CRON_JOB="/usr/bin/php /var/www/t.mercadalibre.live/broadcast_driver.php"
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 --start | --stop"
-    exit 1
-fi
-
-if [ "$1" == "--start" ]; then
-    if crontab -l | grep -q "$CRON_COMMENT"; then
-        (crontab -l | sed "s|$CRON_COMMENT|$CRON_COMMAND|") | crontab -
-        echo "Cron job started!"
-    else
-        echo "Cron job is already running or not set."
-    fi
-elif [ "$1" == "--stop" ]; then
-    if crontab -l | grep -q "$CRON_COMMAND"; then
-        (crontab -l | sed "s|$CRON_COMMAND|$CRON_COMMENT|") | crontab -
-        echo "Cron job stopped!"
-    else
-        echo "Cron job is already stopped."
-    fi
-fi
+case "$1" in
+    --start)
+        if crontab -l | grep -q "# $CRON_JOB"; then
+            (crontab -l | sed "s|# $CRON_JOB|$CRON_JOB|") | crontab -
+            echo "Cron job started!"
+        else
+            echo "Cron job is already running or not set."
+        fi
+        ;;
+    --stop)
+        if crontab -l | grep -q "$CRON_JOB"; then
+            (crontab -l | sed "s|$CRON_JOB|# $CRON_JOB|") | crontab -
+            echo "Cron job stopped!"
+        else
+            echo "Cron job is already stopped."
+        fi
+        ;;
+esac
